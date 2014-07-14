d2fd58cd55bb403c3fc7d747078f1da6a5f36137
pietrogonizzi <user@instant-contiki.(none)>
2014-07-14 10:59:39 +0200

    added java coap server

---
+++ examples-calipso/wos-example/gateway/server.sh
@@ -0,0 +1,1 @@
+java -cp SmartParkingDemo.jar:./lib/californium-0.18.7-final.jar:./lib/cf-server-0.18.7-final.jar:./lib/element-connector-0.2-final.jar it.unipr.iot.calipso.coap.server.process.CalipsoSmartParkingServer

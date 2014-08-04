d4a4b06113684c94d9d1418bb2f3191a0d309152
paolomeda <paolomeda@Napoletano.(none)>
2014-08-04 09:10:12 +0200

    modified uip-ds6 to measure convergence

--- core/net/uip-ds6-route.c
+++ core/net/uip-ds6-route.c
@@ -339,13 +339,11 @@ uip_ds6_route_add(uip_ipaddr_t *ipaddr, uint8_t length,
   memset(&r->state, 0, sizeof(UIP_DS6_ROUTE_STATE_TYPE));
 #endif
 
-  PRINTF("uip_ds6_route_add: adding route: ");
-  PRINT6ADDR(ipaddr);
-  PRINTF(" via ");
-  PRINT6ADDR(nexthop);
-  PRINTF(" prefix len:%u", r->length);
-  PRINTF("\n");
-  ANNOTATE("#L %u 1;blue\n", nexthop->u8[sizeof(uip_ipaddr_t) - 1]);
+  printf("CTIME\t%u\t%u\tNode %u (", ipaddr->u8[sizeof(uip_ipaddr_t) - 1], nexthop->u8[sizeof(uip_ipaddr_t) - 1],ipaddr->u8[sizeof(uip_ipaddr_t) - 1]);
+  print_addr(ipaddr);
+  printf(") has parent %u (", nexthop->u8[sizeof(uip_ipaddr_t) - 1]);
+  print_addr(nexthop);
+  printf(")\n");
 
 #if UIP_DS6_NOTIFICATIONS
   call_route_callback(UIP_DS6_NOTIFICATION_ROUTE_ADD, ipaddr, nexthop);
@@ -355,6 +353,37 @@ uip_ds6_route_add(uip_ipaddr_t *ipaddr, uint8_t length,
   assert_nbr_routes_list_sane();
 #endif /* DEBUG != DEBUG_NONE */
   return r;
+}
+
+void
+print_addr(const uip_ipaddr_t *addr)
+{
+  if(addr == NULL || addr->u8 == NULL) {
+    printf("(NULL IP addr)");
+    return;
+  }
+#if UIP_CONF_IPV6
+  uint16_t a;
+  unsigned int i;
+  int f;
+  for(i = 0, f = 0; i < sizeof(uip_ipaddr_t); i += 2) {
+    a = (addr->u8[i] << 8) + addr->u8[i + 1];
+    if(a == 0 && f >= 0) {
+      if(f++ == 0) {
+        printf("::");
+      }
+    } else {
+      if(f > 0) {
+        f = -1;
+      } else if(i > 0) {
+        printf(":");
+      }
+      printf("%x", a);
+    }
+  }
+#else /* UIP_CONF_IPV6 */
+  PRINTA("%u.%u.%u.%u", addr->u8[0], addr->u8[1], addr->u8[2], addr->u8[3]);
+#endif /* UIP_CONF_IPV6 */
 }
 
 /*---------------------------------------------------------------------------*/
